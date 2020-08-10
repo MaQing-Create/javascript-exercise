40dbe3107480f29d3a027452781209c267948e65
maqing <mr.maqing@gmail.com>
2020-08-10 20:10:16 +0800

    [maqing]finished async task

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,11 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const fetchRes = await fetch(url);
+  if (fetchRes.status === 200) {
+    const json = await fetchRes.json();
+    document.writeln(json.name);
+  } else console.error('Error');
   // end -->
 }
 
